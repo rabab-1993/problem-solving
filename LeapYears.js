6561348edeb6528254f3ea34fda2803a88106cd8
Rabab Yousef <92247904+rabab-1993@users.noreply.github.com>
2021-11-15 22:30:57 +0300

    i tried to solve it

---
+++ LeapYears.js
@@ -0,0 +1,13 @@
+
+function isLeapYear(year) {
+    // TODO
+    if (year / 4) {
+      return false;
+  } else if ( 4/year) {
+    return true;
+  } else {
+    return true;
+  }
+  }
+  
+  isLeapYear(2010);
